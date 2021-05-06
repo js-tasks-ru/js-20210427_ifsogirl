aa482bc988085fcb8720dc748331ceae9614cad3
Холина Дарья Анатольевна <holina.darya@otr.ru>
2021-05-06 12:49:02 +0300

    Task 2 - createGetter, invert object, trim symbols, uniq

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,17 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  let result;
+  if (obj) {
+    result = {};
 
+    Object.entries(obj).forEach(([key, value]) => {
+      result[value] = key;
+    });
+  }
+  else {
+    console.error('Object to invert was not passed');
+  }
+
+  return result;
 }
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,31 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (typeof size !== 'number') {
+    return string;
+  }
 
+  let result = '';
+
+  if (size > 0) {
+    for (let i = 0; i < string.length;) {
+      // запоминаем символ - будем считать кол-во повторений
+      const searchChar = string.charAt(i);
+      let sequenceLength = 0;
+
+      // пока следующие символы совпадают с нашим - увеличиваем счётчик
+      // и добавляем этот символ в результат, пока кол-во  меньше size
+      while (searchChar === string[i]) {
+        i++;
+
+        if (sequenceLength < size) {
+          result += searchChar;
+        }
+
+        sequenceLength++;
+      }
+    }
+  }
+
+  return result;
 }
